2d4432197c110069bf46c9632b360be62add4ee0
Shahrin Fima <fimashahrinakter@gmail.com>
2026-04-20 12:54:01 +0600

    Updated image to local file

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -40,13 +40,7 @@ const Login = () => {
             <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
               {/* Left */}
                 <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-                    <div>
-                        <img
-                            src="https://storage.googleapis.com/devitary-image-host.appspot.com/15846435184459982716-LogoMakr_7POjrN.png"
-                            className="w-32 mx-auto"
-                            alt="Logo"
-                        />
-                    </div>
+                  
 
                     <div className="mt-12 flex flex-col items-center">
                      
@@ -100,7 +94,7 @@ const Login = () => {
                         className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
                         style={{
                             backgroundImage:
-                                "url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg')",
+                                "url('/image.png')",
                         }}
                     ></div>
                 </div>
